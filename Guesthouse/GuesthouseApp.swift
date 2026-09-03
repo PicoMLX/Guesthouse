7d82be4e53ee34ad006b43d370426e34373ecd73
Ronald Mannak <ronaldmannak@me.com>
2026-09-02 21:49:24 -0700

    Initial Commit

---
+++ Guesthouse/GuesthouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuesthouseApp.swift
+//  Guesthouse
+//
+//  Created by Ronald Mannak on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct GuesthouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuesthouseTests/GuesthouseTests.swift
@@ -0,0 +1,18 @@
+//
+//  GuesthouseTests.swift
+//  GuesthouseTests
+//
+//  Created by Ronald Mannak on 9/2/26.
+//
+
+import Testing
+
+struct GuesthouseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
