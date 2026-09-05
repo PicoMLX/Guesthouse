15fe160691dd61d4b0df1f413769f9a1b58faf05
Ronald Mannak <ronaldmannak@me.com>
2026-09-05 00:25:16 -0700

    Adapt probe environment assertion to strict throwing accessor

--- Packages/GuesthouseRuntimeKit/Tests/GuesthouseRuntimeKitTests/LumeBackendTests.swift
+++ Packages/GuesthouseRuntimeKit/Tests/GuesthouseRuntimeKitTests/LumeBackendTests.swift
@@ -152,7 +152,8 @@ private actor LaunchFailingLumeRunner: ProcessRunning {
             ["run", "--detach", "--help"],
             ["attach", "--help"],
         ])
-        #expect(invocations.allSatisfy { $0.environment == storage.environmentForLume() })
+        let expectedEnvironment = try storage.environmentForLume()
+        #expect(invocations.allSatisfy { $0.environment == expectedEnvironment })
         #expect(invocations.allSatisfy { $0.currentDirectory == storage.url(for: .staging) && $0.standardInput == .none })
         #expect(invocations.allSatisfy { $0.maximumOutputBytes == 1 << 20 })
         #expect(invocations.allSatisfy { $0.timeout == .seconds(5) })
