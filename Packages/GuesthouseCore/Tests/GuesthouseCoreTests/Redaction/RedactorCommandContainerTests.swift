8e420bde1cd30ea106cdb7309c5d71773d7d25e3
Ronald Mannak <ronaldmannak@me.com>
2026-09-07 23:43:52 -0700

    Keep container quote regressions in the existing inline companion

---
+++ Packages/GuesthouseCore/Tests/GuesthouseCoreTests/Redaction/RedactorCommandContainerTests.swift
@@ -0,0 +1,24 @@
+import Testing
+@testable import GuesthouseCore
+
+@Suite struct RedactorCommandContainerTests {
+    @Test(arguments: [
+        (#"args=["--password syntheticOpaque"]"#, #"args=["--password [redacted:secret]"]"#),
+        (#"args=["run --password syntheticOpaque"]"#, #"args=["run --password [redacted:secret]"]"#),
+        ("args=['run --password syntheticOpaque']", "args=['run --password [redacted:secret]']"),
+        (#"args=["first", "run --password syntheticOpaque"]"#, #"args=["first", "run --password [redacted:secret]"]"#)
+    ])
+    func containerCommandQuotesCloseWithoutPendingState(_ input: String, _ expected: String) {
+        var state = Redactor.StreamState()
+        #expect(Redactor.redactSecretOptions(input, state: &state) == expected)
+        #expect(state.quotedValue == nil)
+    }
+
+    @Test(arguments: [#"args=["--password syntheticFirst"#, #"args=["run --password syntheticFirst"#,
+                      "args=['run --password syntheticFirst"])
+    func unfinishedContainerCommandsRetainQuoteState(_ input: String) {
+        var state = Redactor.StreamState()
+        #expect(!Redactor.redactSecretOptions(input, state: &state).contains("syntheticFirst"))
+        #expect(state.quotedValue != nil)
+    }
+}
